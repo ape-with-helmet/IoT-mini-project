114bb56622576ac2d43500c82657cb4c15dc04cc
Sadhgun <ganeshshatrugna@gmail.com>
2024-02-12 22:51:34 +0530

    basic setup donwe

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -9,3 +9,4 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+
--- mornon/src/App.js
+++ mornon/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Navbar } from './Components/Navbar';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Homepage from './Pages/Homepage'
+import About from './Pages/About';
 
 function App() {
   return (
     <>
       <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route element={<Login/>} path='/login'/>
+        <Route element={<Signup/>} path='/signup'/>
+        <Route element={<Homepage/>} path='/'/>
+        <Route element={<About/>} path='/about'/>
+      </Routes>
       </BrowserRouter>
     </>
   );
