c628f81a8c0b656ca7a18195e5a56079ef261c25
Sadhgun <ganeshshatrugna@gmail.com>
2024-02-12 22:32:09 +0530

    added corss

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,4 +1,11 @@
+const express = require("express")
+const cors = require("cors")
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ganeshshatrugna:rj8DyBUfzjaTdO2k@mornon.gdze2yg.mongodb.net/userdetails')
     .then(() => { console.log("Database gg connected") })
     .catch((error) => { console.log(error) })
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
