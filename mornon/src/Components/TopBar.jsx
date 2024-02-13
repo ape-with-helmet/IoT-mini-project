297f7608aaca589c1e63a315a8644544334ba84d
Sadhgun <ganeshshatrugna@gmail.com>
2024-02-14 01:16:43 +0530

    added typing effect hook

--- mornon/src/Components/TopBar.jsx
+++ mornon/src/Components/TopBar.jsx
@@ -1,34 +1,22 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from 'react'
 import '../Components/TopBar.css'
 
 const TopBar = () => {
     return (
-        <div className="container-fluid background_navbar">
-            <Navbar expand="lg" className="bg-body-dark">
+        <div className="container-fluid bg-dark">
+            <Navbar fixed='top' expand="lg" className="bg-body" bg='dark' data-bs-theme="dark">
                 <Container >
                     <Navbar.Brand href="#home"><span className='topbar_logo'>WaterWatch</span></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home"><span className='home_nav'>Home</span></Nav.Link>
-                            <Nav.Link href="#link"><span className='home_nav'>Link</span></Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
+                    <Nav className="me-auto-dark">
+                        <Nav.Link href="#home"><span className='home_nav'>About Us</span></Nav.Link>
+                        {/* <Nav className='justify-content-end'> */}
+                        <Nav.Link href="#link"><span className='home_nav'>Log In</span></Nav.Link>
+                        <Nav.Link href="#link"><span className='home_nav'>Sign Up</span></Nav.Link>     
+                        {/* </Nav> */}
+                    </Nav>
                 </Container>
             </Navbar>
         </div>
--- mornon/src/Pages/About.jsx
+++ mornon/src/Pages/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './About.css'
+import { useTypingEffect } from '../hooks/typing-effect';
 
 const About = () => {
+  const text = useTypingEffect("Hello i am under the water!",50);
   return (
-    <div>About</div>
+    <div className='container'>
+      <p className='about_text'>
+        {text}
+      </p>
+    </div>
   )
 }
 
