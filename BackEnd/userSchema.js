da96fe51cdd6ad080c41bacf7e41ed624f3b9468
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2024-11-17 12:50:36 +0530

    Update userSchema.js

--- BackEnd/userSchema.js
+++ BackEnd/userSchema.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://ganeshshatrugna:rj8DyBUfzjaTdO2k@mornon.gdze2yg.mongodb.net/userdetails', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('<Connection String here>', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log('Database connected') })
     .catch((error) => { console.error('Error connecting to MongoDB:', error) });
 const userSchema = new mongoose.Schema({
